f57f6434af10bff7cf45cb3b55ca972c50ebbec2
cjosti <cleitonosti@gmail.com>
2020-03-29 14:00:26 -0300

    adjusts

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,29 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/');
+
+describe('ONG', () => {
+  beforeEach(async () => {
+    await connection.migrate.rollback();
+    await connection.migrate.latest();
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  it('should be able to create a new ONG', async () => {
+    const response = await request(app)
+      .post('/ongs')
+      .send({
+        name: 'Teste',
+        email: 'contato@teste.com.br',
+        whatsapp: '19000000000',
+        city: 'Piracicaba',
+        uf: 'SP'
+      });
+
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.id).toHaveLength(8);
+  });
+});
